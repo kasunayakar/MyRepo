a375f9df8c08fe0eac06c9301668a487e83eaf9e
user1 <user1@sathya.com>
2016-01-13 18:56:35 +0530

    one sop statement added by user1 on 13-1-16

--- Test.java
+++ Test.java
@@ -5,5 +5,6 @@ class Test
 		System.out.println(" Hello Git Hub ");
 		System.out.println("data is added");
 		System.out.println("third commit added");
+		System.out.println("happy sankranthi");
 	}
 }
